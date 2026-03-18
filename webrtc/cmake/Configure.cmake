222b559b7037fea3c797750eda0b8905925b05cc
sanketdiwale <sanketdiwale@gmail.com>
2026-03-18 04:47:43 -0400

    fixed args for mac build

--- webrtc/cmake/Configure.cmake
+++ webrtc/cmake/Configure.cmake
@@ -3,20 +3,24 @@ if(APPLE)
     set(CMAKE_OSX_SYSROOT macosx10.14)
 endif(APPLE)
 
-set(WEBRTC_SYSROOT_COMMAND python build/linux/sysroot_scripts/install-sysroot.py --arch=${WEBRTC_ARCH})
-webrtc_command(
-    NAME sysroot
-    COMMAND ${WEBRTC_SYSROOT_COMMAND}
-    WORKING_DIRECTORY ${WEBRTC_FOLDER}/src
-    DEPENDS download
-)
+set(WEBRTC_GEN_DEPENDS download)
+if(UNIX AND NOT APPLE)
+    set(WEBRTC_SYSROOT_COMMAND python build/linux/sysroot_scripts/install-sysroot.py --arch=${WEBRTC_ARCH})
+    webrtc_command(
+        NAME sysroot
+        COMMAND ${WEBRTC_SYSROOT_COMMAND}
+        WORKING_DIRECTORY ${WEBRTC_FOLDER}/src
+        DEPENDS download
+    )
+    list(APPEND WEBRTC_GEN_DEPENDS sysroot)
+endif()
 
 set(WEBRTC_CONFIGURE_COMMAND gn gen out/${WEBRTC_BUILD_TYPE} --args=${WEBRTC_GEN_ARGS})
 webrtc_command(
     NAME gen
     COMMAND ${WEBRTC_CONFIGURE_COMMAND}
     WORKING_DIRECTORY ${WEBRTC_FOLDER}/src
-    DEPENDS sysroot
+    DEPENDS ${WEBRTC_GEN_DEPENDS}
 )
 
 webrtc_command(
